b8145e5cf705af992f350626ee6bdf1338a56092
christopher <cloudynginya@gmail.com>
2025-05-17 05:52:22 -0700

    changes

--- script.js
+++ script.js
@@ -2,12 +2,29 @@
 // select DOM elements
 const addMealInput = document.getElementById("Add-meal");
 const addCalorieInput = document.getElementById("Add-calorie");
+const addmealbutton = document.getElementById("meal-button");
+const caloriestable = document.getElementById("calorie-table");
 const caloriestablebody = document.getElementById("calorie-table-body");
 
 
 //functions
 
 function addMealInput() {
-    const meal = mea
+    const meal = mealInput.value.trim();
+    const calories = calorieInput.value.trim();
+    const date = dateInput.value.trim();
 
+
+   meals.push({ meal, calories, date });
+   mealinput.value = '';
+   calorieinput.value = '';
+   dateinput.value = '';
+   updateTable();
 }
+  function deleteMeal(index) {
+    meals.slice(index, 1);
+    updateTable();
+ }
+
+ document.getElementById('Add-Meal-Button').addEventListener('click', addMeal);
+  document.getElementById('Add-Calorie-Button').addEventListener('click', addMeal);
