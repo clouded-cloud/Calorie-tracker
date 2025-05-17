997d46c773cf3d9e6a682e358f07750ab3f1be54
christopher <cloudynginya@gmail.com>
2025-05-17 04:01:42 -0700

    script

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const addMealInput = document.getElementById("Add-meal");
 const addMealButton = document.getElementById("Add-Meal-Button");
 const addCalorie = document.getElementById("Add-calorie");
 const addCalorieButton = document.getElementById("Add-Calorie-Button");
-const addMeal = document.getElementById("add meal");
+const  = document.getElementById("add meal");
