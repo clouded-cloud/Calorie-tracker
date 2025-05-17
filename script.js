799a3f877dfb7d376b48819dcbef21809ef09b68
christopher <cloudynginya@gmail.com>
2025-05-17 03:41:49 -0700

    script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // select DOM elements
-const addMeal = document.getElementById("add meal");
+const addMealInput = document.getElementById("add meal");
+const addMealButton = document.getElementById("add meal");
 const addCalorie = document.getElementById("add calorie");
-const addMeal = document.getElementById("add meal");
 const addMeal = document.getElementById("add meal");
 const addMeal = document.getElementById("add meal");
